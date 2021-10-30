f653650c3cb49004813dafe870ccd4e9b975ae45
BerkanSe <sengerberkann@gmail.com>
2021-10-30 17:41:38 +0300

    Ders sonu

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhaba Kodlama.io")
+
+const euroDun = 11.2
+//euroDun = 10
+
+let konutKredileri = ["Konut kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"] 
+
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length; i++) {
+    console.log("<li>"+ konutKredileri[i] + "</li")   
+}
+console.log("</ul>")
+
+// console.log(konutKredileri)
